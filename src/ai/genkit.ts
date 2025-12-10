de333cec75a88f3474b9020ef8484a83125ef60e
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2025-12-10 12:35:26 +0000

    reinic a la palicacion ya no esta funcionando por favor

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import { next } from '@genkit-ai/next/plugin';
+import { next } from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
