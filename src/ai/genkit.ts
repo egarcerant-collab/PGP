0c61cc714893273d41f8511718328d7f8415700e
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2025-12-17 20:10:36 +0000

    cual cuades usar

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -11,7 +11,7 @@ export const ai = genkit({
     }),
   ],
   model: {
-    name: 'gemini-1.5-flash-latest',
+    name: 'googleai/gemini-2.5-pro',
     temperature: 0.4,
     maxOutputTokens: 2048,
   },
