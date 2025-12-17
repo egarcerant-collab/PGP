c8b11fea488f2c277d27d4bd837b87102d4a955d
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2025-12-17 20:04:21 +0000

    peudes implermarte en gvcamnbio un alaisis con gemimie 2.0

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,11 +7,11 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY,
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   model: {
-    name: 'gemini-1.5-pro',
+    name: 'gemini-1.5-flash-latest',
     temperature: 0.4,
     maxOutputTokens: 2048,
   },
